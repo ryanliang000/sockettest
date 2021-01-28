881b903ae28cc1921bdcdd91f9f61caff1b7202a
ryanliang000 <mail>
2021-01-28 21:43:03 +0800

    fix kqueue

--- tools/include/sockproc.h
+++ tools/include/sockproc.h
@@ -1,22 +1,22 @@
 #ifndef TBUFF_LENGTH 
-#define TBUFF_LENGTH 1240
+#define TBUFF_LENGTH 65536
 #endif
 #include "log.h"
 #include "pub.h"
 struct tbuff{
 	char buff[TBUFF_LENGTH];
-	char recvn;
-	char sendn;
+	int recvn;
+	int sendn;
 	tbuff():recvn(0),sendn(0){}
 	void reset(){recvn=0;sendn=0;}
 	int leftlen(){return recvn > sendn ? recvn - sendn : 0;}
-	char* leftbuff(){return recvn > sendn ? buff+leftlen(): NULL;}
+	char* leftbuff(){return recvn > sendn ? buff+sendn: NULL;}
 };
-enum sock_flag
+enum sockflag
 {
-	server_flag = 0,
-	client_flag = 1,
-	remote_flag = 2
+	sock_server = 0,
+	sock_client = 1,
+	sock_remote = 2
 };
 struct tsock{
 	int fd;
@@ -24,15 +24,16 @@ struct tsock{
 	tbuff tbuf;
 	int flag; // 0-server, 1-client, 2-remote
 	tsock():fd(-1),dstfd(-1),flag(-1){}
+	tsock(int _fd, int _dt, int _f):fd(_fd),dstfd(_dt),flag(_f){}
 	void reset(){fd=-1;dstfd=-1;flag=-1;tbuf.reset();}
-	void setfd(int _fd, int _dstfd){fd=_fd, dstfd=_dstfd;}
+	void setfd(int _fd, int _dstfd, int _flag=-1){fd=_fd, dstfd=_dstfd;flag=_flag;}
 	void setdst(int _dstfd){dstfd=_dstfd;}
 	void setflag(int _flag){flag=_flag;}
 	const char* desc(){
 		if(flag==1) return "client";
 		else if(flag==2) return "remote";
 		else if(flag==0) return "server";
-		return "";
+		return "undefine flag";
 	}
 };
 
@@ -43,18 +44,26 @@ struct tsock{
 int recvsock(tsock& info)
 {
     tbuff& tbuf = info.tbuf;
+	tbuf.reset();
 	int fd = info.fd;
-    if ((tbuf.recvn = recv(fd, tbuf.buff, TBUFF_LENGTH, 0)) < 0){ 
-        LOG_E("recvsockandsend: recv fd[%d] error[%d-%s]", fd, errno, strerror(errno));
+    tbuf.recvn = recv(fd, tbuf.buff, TBUFF_LENGTH, 0);
+	if (tbuf.recvn < 0){ 
+        if (errno == EAGAIN || errno == EWOULDBLOCK){
+			tbuf.reset();
+			return 1;
+		}
+		LOG_E("recvsock: recv fd[%d] rt[%d] error[%d-%s]", fd, tbuf.recvn, errno, strerror(errno));
         return -1; 
     }   
     if (tbuf.recvn == 0){ 
         if (errno == EAGAIN || errno == EWOULDBLOCK){
+			tbuf.reset();
 			return 1;
 		}
 		LOG_I("recvsockandsend: close by %s fd[%d]", info.desc(), fd);
 		return -2; 
     }
+	//LOG_I("recv msg: %s", buffer2hex(tbuf.buff, tbuf.recvn));
 	return 0;
 }
 // 0: send message succ
@@ -67,6 +76,7 @@ int sendsock(tsock& info)
 	if (buff == NULL)
 		return 0;
 	int left = tbuf.leftlen();
+	//LOG_I("send msg: %s", buffer2hex(buff, left));
 	int num = send(info.dstfd, buff, left, 0);
 	if (num == left){
 		tbuf.sendn = tbuf.recvn;
@@ -93,17 +103,23 @@ int recvsockandsendencoded(tsock& info, int key)
     int fd = info.fd;
     int dst = info.dstfd;
 	int rt = 0;
+	int n = 0;
     do{
-		tbuf.reset();
+		n++;
 		if ((rt = recvsock(info)) < 0){ 
 			return rt;
 		}
+        if (rt == 1 && n == 1){
+			LOG_E("first recv msg empty(%d->%d), close conn", fd, dst);
+			return -1;
+		}
+		LOG_D("recvandsend: (%d->%d) rt-%d len-%d n-%d", fd, dst, rt, tbuf.recvn,n);
 		if (rt == 1) return 0; 
 		encodebuffer(tbuf.buff, tbuf.recvn, key);
 		if ((rt = sendsock(info)) != 0){
 			return rt;
 		}
-    }while(tbuf.recvn == tbuf.sendn);
+    }while(tbuf.recvn == TBUFF_LENGTH);
     return 0;
 }
 
